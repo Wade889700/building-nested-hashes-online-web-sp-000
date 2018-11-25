5b823dcb5142b7e056b997a15c2815e988841a7a
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-25 11:00:12 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,15 +1,15 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      patriarch:{},
-      matriarch:{},
-      heroine:{},
-      hero_friends:[],
+      :patriarch=>{},
+      :matriarch=>{},
+      :heroine=>{},
+      :hero_friends=>[],
       },
     :capulet => {
-      patriarch:{},
-      matriarch:{},
-      heroine:{},
-      hero_friends:[],
+      :patriarch=>{},
+      :matriarch=>{},
+      :heroine=>{},
+      :hero_friends=>[],
       }}
 end
